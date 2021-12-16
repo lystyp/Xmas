773a5eff322b066af5738dfb7d4b7b8ae5768aa8
lystyp <lystyp87015@gmail.com>
2021-12-16 15:32:00 +0800

    Ａdd socket.io

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-var http = require("http");
+const express = require('express');
+const app = express();
 
-http.createServer(function(request, response) {
-  console.log("Server is connected.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello world");
-  response.end();
-}).listen(process.env.PORT || 8000);
-console.log("Server is created.");
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+ 
+app.get('/', (req, res) => {
+  console.log('get request !'); 
+  res.sendFile( __dirname + '/chat.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('socket connection !'); 
+ 
+    socket.on('disconnect', () => {
+        console.log('socket disconnect~'); 
+    });
+
+    socket.on("send", (msgInfo) => {
+      console.log('send' + msgInfo);
+      socket.emit("newMsg", msgInfo);
+    });
+});
+ 
+server.listen(process.env.PORT || 8000, () => {
+  console.log("Server created.")   
+});
+ 
