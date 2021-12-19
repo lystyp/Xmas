489ea87da4e21b06046a0b57610166424f74e391
lystyp30063 <lystyp30063@gmail.com>
2021-12-20 01:06:25 +0800

    Add login and manager page.

--- main.js
+++ main.js
@@ -5,20 +5,33 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
  
 app.get('/', (req, res) => {
-  console.log('get request !'); 
+  console.log('login !'); 
+  res.sendFile( __dirname + '/login.html');
+});
+
+app.get('/user', (req, res) => {
+  console.log('get user request, name = ' + req.query.name); 
   res.sendFile( __dirname + '/chat.html');
 });
 
+app.get('/manager', (req, res) => {
+  console.log('get manager request'); 
+  res.sendFile( __dirname + '/manager.html');
+});
+
+let clients = [];
+let sendMsg = function(msgInfo) {
+  for (let c of clients) {
+    c.emit("newMsg", msgInfo);
+  }
+}
+
 io.on('connection', (socket) => {
     console.log('socket connection !'); 
- 
-    socket.on('disconnect', () => {
-        console.log('socket disconnect~'); 
-    });
-
+    clients.push(socket);
     socket.on("send", (msgInfo) => {
-      console.log('send' + msgInfo);
-      socket.emit("newMsg", msgInfo);
+      console.log('send, user = ' + msgInfo.name + ", msg = " + msgInfo.msg);
+      sendMsg(msgInfo);
     });
 });
  
@@ -26,4 +39,3 @@ server.listen(process.env.PORT || 8000, () => {
   console.log("Server created.")   
 });
 
-//git push test
