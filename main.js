239022d8d1b2f3d4bb1ff9dac336561697c1f5ec
lystyp <lystyp87015@gmail.com>
2021-12-21 00:52:30 +0800

    Finish first version.

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ let basic_points = 1
 var answer = ""
 var rank = 0
 var userMap = {}
+var currentImage = ""
+var answeredUser = {};
 
 app.get('/', (req, res) => {
   console.log('login !'); 
@@ -19,12 +21,17 @@ app.get('/', (req, res) => {
 
 app.get('/user', (req, res) => {
   console.log('get user request, name = ' + req.query.name); 
-  userMap[req.query.name] = 
-  {
-    name: req.query.name,
-    score: 0
-  }
   res.sendFile( __dirname + '/chat.html');
+});
+
+app.get('/reset', (req, res) => {
+  console.log('reset'); 
+  answer = ""
+  rank = 0
+  userMap = {}
+  currentImage = ""
+  answeredUser = {};
+  res.sendFile( __dirname + '/manager.html');
 });
 
 app.get('/manager', (req, res) => {
@@ -45,6 +52,12 @@ let sendMsg = function(msgInfo) {
   }
 }
 
+let userScoreChanged = function() {
+  for (let c of clients) {
+    c.emit("userScoreChanged", userMap);
+  }
+}
+
 io.on('connection', (socket) => {
     console.log('socket connection !'); 
     clients.push(socket);
@@ -52,29 +65,53 @@ io.on('connection', (socket) => {
     socket.on("send", (msgInfo) => {
       console.log('send, user = ' + msgInfo.name + ", msg = " + msgInfo.msg);
       if(msgInfo.msg == answer) {
-        if(rank < RANK_MAX) {
-          userMap[msgInfo.name].score = userMap[msgInfo.name].score + reward_points[rank]
-        }else {
-          userMap[msgInfo.name].score = userMap[msgInfo.name].score + basic_points
+        if (answeredUser[answer].includes(msgInfo.name) ) {
+          msgInfo.msg = "不要洗分數!!!"
+        } else {
+          msgInfo.msg = "答對囉!!!"
+          if (answeredUser[answer])
+            answeredUser[answer].push(msgInfo.name);
+          else
+            answeredUser[answer] = [msgInfo.name];
+          for (let c of clients) {
+            c.emit("someoneAnswered", answeredUser);
+          }
+
+          if(rank < RANK_MAX) {
+            userMap[msgInfo.name].score = userMap[msgInfo.name].score + reward_points[rank]
+          }else {
+            userMap[msgInfo.name].score = userMap[msgInfo.name].score + basic_points
+          }
+          rank++
+          userScoreChanged();
         }
-        msgInfo.msg = "答對囉!!!"
-        rank++
       }
       console.log("score: " + userMap[msgInfo.name].score)
       sendMsg(msgInfo);
     });
 
-    socket.on("send_answer", (foreground_answer) => {
-      console.log('send_answer, answer = ' + foreground_answer);
-      answer = foreground_answer
+    socket.on("next_round", (data) => {
+      console.log('next_round : ' + JSON.stringify(data));
+      currentImage = data.image;
+      answer = data.answer;
+      answeredUser[answer] = [];
+      rank = 0;
+      for (let c of clients) {
+        c.emit("next_round", currentImage);
+      }
     });
 
-    socket.on("next_round", (image_number) => {
-      //clear msg and change image
-      console.log('next_round, image_number = ' + image_number);
-      for (let c of clients) {
-        c.emit("imageMsg", "/image?image_name=" + image_number + ".png");
+    socket.on("joinGame", (name) => {
+      console.log('joinGame :' + name);
+      if (name != null && name != undefined && !userMap[name] ) {
+        userMap[name] = 
+        {
+          name: name,
+          score: 0,
+        }
+        userScoreChanged();
       }
+      socket.emit("initData", {image:currentImage, score:userMap});
     });
 });
  
